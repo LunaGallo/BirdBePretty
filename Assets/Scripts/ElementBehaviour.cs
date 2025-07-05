7b88c05ad96852880cd81e61f55f86d9306366d8
Luna Gallo <lunamgallo@gmail.com>
2025-07-05 16:47:26 -0300

    Criando mais uns scripts de base

--- Assets/Scripts/ElementBehaviour.cs
+++ Assets/Scripts/ElementBehaviour.cs
@@ -1,12 +1,18 @@
+using LunaLib;
 using Shapes;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class ElementBehaviour : MonoBehaviour {
+public class ElementBehaviour : SingletonGroup<ElementBehaviour> {
 
     public Vector2Int gridSize = Vector2Int.one;
     public Rectangle rectagleGizmo;
     public SpriteRenderer spriteRenderer;
     public float ghostAlpha = 0.5f;
     public ElementData data;
+
+    public bool IsFixed { get; set; } = false;
+    public IEnumerable<ElementBehaviour> AllFixed => InstanceList.Where(i => i.IsFixed);
 
 }
